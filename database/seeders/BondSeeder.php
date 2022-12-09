93bbe7c9106ce1ff3b68995d9362b814b55698f2
selminazkerimli <47450005+kselminaz@users.noreply.github.com>
2022-12-09 15:54:47 +0400

    changes for readme.md and install.md

--- database/seeders/BondSeeder.php
+++ database/seeders/BondSeeder.php
@@ -16,9 +16,9 @@ class BondSeeder extends Seeder
     {
 
         DB::table('bonds')->insert([
-            ['issue_date' => '2021-11-08', 'last_circulation_date' => '2022-12-30', 'nominal_price' => 100, 'coupon_payout_frequency' => '4','interest_calculation_period'=>'360','coupon_interest'=>10],
-            ['issue_date' => '2021-11-08', 'last_circulation_date' => '2022-12-30', 'nominal_price' => 200, 'coupon_payout_frequency' => '4','interest_calculation_period'=>'364','coupon_interest'=>20],
-            ['issue_date' => '2021-11-08', 'last_circulation_date' => '2022-12-31', 'nominal_price' => 300, 'coupon_payout_frequency' => '4','interest_calculation_period'=>'365','coupon_interest'=>30],
+            ['issue_date' => '2021-01-08', 'last_circulation_date' => '2022-12-30', 'nominal_price' => 100, 'coupon_payout_frequency' => '4','interest_calculation_period'=>'360','coupon_interest'=>10],
+            ['issue_date' => '2021-10-08', 'last_circulation_date' => '2022-12-30', 'nominal_price' => 200, 'coupon_payout_frequency' => '4','interest_calculation_period'=>'364','coupon_interest'=>20],
+            ['issue_date' => '2022-01-08', 'last_circulation_date' => '2022-12-31', 'nominal_price' => 300, 'coupon_payout_frequency' => '4','interest_calculation_period'=>'365','coupon_interest'=>30],
 
         ]);
     }
--- install.md
+++ install.md
@@ -1,17 +1,17 @@
 1. Clone GitHub repo for this project locally
-- git clone https://github.com/kselminaz/algorithma_task_bond.git
+   git clone https://github.com/kselminaz/algorithma_task_bond.git
 2. Remember to type cd projectName to move your terminal working location to the project file
 3. Install Composer Dependencies
-- composer install
+   composer install
 4. Create a copy of your .env file
-- cp .env.example .env
+   cp .env.example .env
 5. Generate an app encryption key
-- php artisan key:generate
+   php artisan key:generate
 6. Create an empty database for the app in mysql
 7. Migrate the database
-- php artisan migrate
+   php artisan migrate
 8. Seed the database
-- php artisan db:seed
+   php artisan db:seed
 
 
 
